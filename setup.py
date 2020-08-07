5d3d87da3bd7cae2d0a0f007b434ee0e249cb956
Chris Pattison <chpattison@gmail.com>
2020-08-06 17:40:29 -0700

    package info

--- setup.py
+++ setup.py
@@ -2,6 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
   name='rmview',
+  version='0.1',
+  url='https://github.com/bordaigorl/rmview',
+  description='rMview: a fast live viewer for reMarkable',
+  author='bordaigorl',
+  author_email='emanuele.dosualdo@gmail.com',
+  classifiers=[
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+  ],
   packages=['rmview'],
   install_requires=['pyqt5', 'paramiko', 'twisted'],
   entry_points={
