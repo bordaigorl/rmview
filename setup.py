e7c3dff3b18b433072079d2432d423c547925dc2
Théo Reichel <theo@nolog.ch>
2021-09-11 23:28:37 +0200

    Add pyOpenSSL dependency in setup.py
    On a fresh `ubuntu 20.04` install with `python3` (and venv), starting `rmview` fails due to the missing _OpenSSL_ dependency.
    
    ```
    Traceback (most recent call last):
      File "/opt/rmview/.venv/bin/rmview", line 5, in <module>
        from rmview.rmview import rmViewMain
      File "/opt/rmview/.venv/lib/python3.8/site-packages/rmview/rmview.py", line 8, in <module>
        from .screenstream.screenshare import ScreenShareStream
      File "/opt/rmview/.venv/lib/python3.8/site-packages/rmview/screenstream/screenshare.py", line 16, in <module>
        from twisted.internet import protocol, reactor, ssl
      File "/opt/rmview/.venv/lib/python3.8/site-packages/twisted/internet/ssl.py", line 58, in <module>
        from OpenSSL import SSL  # type: ignore[import]
    ModuleNotFoundError: No module named 'OpenSSL'
    ```
    
    Steps to reproduce the issue:
    ```
    $ pyton3 -m venv .venv
    $ source .venv/bin/activate
    $ pip install .
    $ rmview
    ```
    Installing SSL lib with `pip install pyOpenSSL` will fix the issue.

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
   ],
   packages=['rmview', 'rmview.screenstream'],
-  install_requires=['pyqt5', 'paramiko', 'twisted'],
+  install_requires=['pyqt5', 'paramiko', 'twisted', 'pyOpenSSL'],
   extras_require = { 'tunnel': ['sshtunnel'] },
   entry_points={
     'console_scripts':['rmview = rmview.rmview:rmViewMain']
